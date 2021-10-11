3d2200e04e6508e3a65ed4e5210e0d54cc6a7b4d
v-mikedu <v-mikedu@ANDY09WIN11>
2021-10-11 08:21:25 +0000

    Add project files.

---
+++ Dev17.0 RC/Anc50_MVCWithRCL_Ind/Anc50_MVCWithRCL_Ind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anc50_MVCWithRCL_Ind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
